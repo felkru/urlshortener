10052b95bb481f8413db25d81d4c692bf193f2cf
Felix Krückel <64913698+felkru@users.noreply.github.com>
2023-01-26 16:26:26 +0000

    successfully completed challenge

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const dns = require("dns");
+const bodyParser = require("body-parser");
 const app = express();
+let urls = {};
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors(), bodyParser.urlencoded({ extended: false }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/shorturl/:num", function (req, res) {
+  if (!urls[req.params.num]) return res.json({ error: "invalid URL" });
+  res.redirect(urls[req.params.num]);
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl/", function (req, res) {
+  let url = req.body.url;
+  // check if url is valid
+  if (!url.match(/^(http|https):\/\/.*\..*$/))
+    return res.json({ error: "invalid URL" });
+  // if (!url.match(/^(http|https):\/\//)) url = "https://" + url;
+  // when urls contains the url, return the short url
+  let currentkey = Object.values(urls).indexOf(url);
+  if (currentkey > -1) {
+    res.json({ original_url: urls[currentkey], short_url: currentkey });
+    return;
+  }
+  urls[Object.keys(urls).length] = url;
+  let num = Object.keys(urls).length - 1;
+  res.json({ original_url: url, short_url: num });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
