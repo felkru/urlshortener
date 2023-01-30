f259705077c1ea300ae4884f8251179d87d9bd55
Felix Krückel <64913698+felkru@users.noreply.github.com>
2023-01-30 06:54:37 +0000

    shortened endpoints, added cloud sync

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const express = require("express");
 const cors = require("cors");
 const dns = require("dns");
 const bodyParser = require("body-parser");
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(process.env.db_uri);
+const database = client.db("felkrucom");
+const dburls = database.collection("short_urls");
 const app = express();
 let urls = {};
+
+async function fetchRedirects() {
+  try {
+    urls = await dburls.findOne({ function: "sync urls" });
+    console.log(urls);
+  } catch {
+    // Ensures that the client will close when you finish/error
+    console.log("error");
+  }
+}
+fetchRedirects();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,18 +32,21 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.get("/redirects", function (req, res) {
+  res.json(urls);
+});
+
 // Your first API endpoint
-app.get("/api/shorturl/:num", function (req, res) {
+app.get("/:num", function (req, res) {
   if (!urls[req.params.num]) return res.json({ error: "invalid URL" });
   res.redirect(urls[req.params.num]);
 });
 
-app.post("/api/shorturl/", function (req, res) {
+app.post("/add", function (req, res) {
   let url = req.body.url;
   // check if url is valid
-  if (!url.match(/^(http|https):\/\/.*\..*$/))
-    return res.json({ error: "invalid URL" });
-  // if (!url.match(/^(http|https):\/\//)) url = "https://" + url;
+  if (!url.match(/^(http|https):\/\//)) url = "https://" + url;
+  if (!url.match(/.*\..*$/)) return res.json({ error: "invalid URL" });
   // when urls contains the url, return the short url
   let currentkey = Object.values(urls).indexOf(url);
   if (currentkey > -1) {
@@ -36,7 +54,16 @@ app.post("/api/shorturl/", function (req, res) {
     return;
   }
   urls[Object.keys(urls).length] = url;
-  let num = Object.keys(urls).length - 1;
+  let num = Object.keys(urls).length - 3;
+  // save urls to database
+  dburls.updateOne(
+    { function: "sync urls" },
+    {
+      $set: {
+        [num]: url,
+      },
+    }
+  );
   res.json({ original_url: url, short_url: num });
 });
 
